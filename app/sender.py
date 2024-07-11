bf9fb29fae469383e7c29974e9a0302b5919a9b4
Isaac <isaacgalmeida@hotmail.com>
2024-07-11 20:28:22 -0300

    update

--- app/sender.py
+++ app/sender.py
@@ -4,7 +4,6 @@ import json
 import os
 from bottle import Bottle, request
 
-
 class Sender(Bottle):
     def __init__(self):
         super().__init__()
@@ -15,8 +14,9 @@ class Sender(Bottle):
 
         db_host = os.getenv('DB_HOST', 'db')
         db_user = os.getenv('DB_USER', 'postgres')
-        db_name = os.getenv('DB_NAME', 'sender')
-        dsn = f'dbname={db_name} user={db_user} host={db_host}'
+        db_pass = os.getenv('DB_PASS', 'password')
+        db_name = os.getenv('DB_NAME', 'email_sender')
+        dsn = f'dbname={db_name} user={db_user} password={db_pass} host={db_host}'
         self.conn = psycopg2.connect(dsn)
         
     def register_message(self, assunto, mensagem):
